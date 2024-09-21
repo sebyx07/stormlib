3fb8106a438f0e6e89265814eebe9dc8300a1728
sebi <gore.sebyx@yahoo.com>
2024-09-21 16:19:55 +0100

    update

--- ext/stormlib/extconf.rb
+++ ext/stormlib/extconf.rb
@@ -1,24 +1,34 @@
 # frozen_string_literal: true
 
 require 'mkmf'
+require 'fileutils'
 
-puts 'Checking for StormLib...'
+def log(message)
+  puts "[extconf.rb] #{message}"
+end
+
+log 'Checking for StormLib...'
 STORMLIB_DIR = File.expand_path('../StormLib', __FILE__)
 
 if File.directory?(STORMLIB_DIR) && File.exist?(File.join(STORMLIB_DIR, 'src', 'StormLib.h'))
-  puts 'StormLib found!'
+  log 'StormLib found!'
 else
   raise "Cannot find StormLib directory in #{STORMLIB_DIR}"
 end
 
+# Compile StormLib
 Dir.chdir(STORMLIB_DIR) do
-  # Configure CMake to use -fPIC
-  system('cmake . -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_C_FLAGS="-Wno-old-style-definition"')
-  system('make clean')  # Clean previous build
-  system('make')
+  log 'Configuring StormLib with CMake...'
+  system('cmake . -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_C_FLAGS="-Wno-old-style-definition" -DBUILD_SHARED_LIBS=OFF') or raise 'Failed to configure StormLib with CMake'
+
+  log 'Cleaning previous build...'
+  system('make clean') or log 'Warning: make clean failed, but continuing...'
+
+  log 'Building StormLib...'
+  system('make') or raise 'Failed to build StormLib'
 end
 
-# Find the actual library file
+# Find the library file
 lib_file = Dir.glob("#{STORMLIB_DIR}/**/{libstorm.a,libstorm.so}").first
 
 if lib_file.nil?
@@ -28,18 +38,36 @@ end
 lib_dir = File.dirname(lib_file)
 lib_name = File.basename(lib_file).sub(/^lib/, '').sub(/\.(a|so)$/, '')
 
+log "Found StormLib: #{lib_file}"
+
 # Set up the extension
 $CFLAGS << " -I#{STORMLIB_DIR}/src -fPIC"
-$LDFLAGS << " -L#{lib_dir} -l#{lib_name} -lz -lbz2"
+$LDFLAGS << " -L#{lib_dir} -l#{lib_name} -lz -lbz2 -lstdc++"
 
-# On some systems, you might need to adjust the library path
+# Adjust the library path
 $LDFLAGS << " -Wl,-rpath,#{lib_dir}"
 
-# Make sure the compiler can find the necessary headers and libraries
+# Ensure the compiler can find necessary headers and libraries
 dir_config('stormlib', "#{STORMLIB_DIR}/src", lib_dir)
 
-have_library(lib_name) or raise "Unable to find #{lib_name} library"
-have_library('z') or raise "Unable to find zlib"
-have_library('bz2') or raise "Unable to find bzip2 library"
+unless have_library(lib_name)
+  log "Unable to find #{lib_name} library. Contents of #{lib_dir}:"
+  Dir.foreach(lib_dir) { |f| log "  #{f}" }
+  raise "Unable to find #{lib_name} library"
+end
 
+unless have_library('z')
+  log 'Trying to find zlib in system directories...'
+  raise 'Unable to find zlib' unless find_library('z', 'deflate')
+end
+
+unless have_library('bz2')
+  log 'Trying to find bzip2 in system directories...'
+  raise 'Unable to find bzip2 library' unless find_library('bz2', 'BZ2_bzCompress')
+end
+
+# Create the makefile
+log 'Creating Makefile...'
 create_makefile('stormlib/stormlib')
+
+log 'extconf.rb completed successfully'
